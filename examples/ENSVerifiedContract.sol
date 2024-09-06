5cd7cefe1a98f3de15c092cc18afee7a42fb0675
madgeniusblink <madgeniusblink@gmail.com>
2024-09-05 20:24:25 -0700

    removed example folder

--- examples/ENSVerifiedContract.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
-
-import {ENSMetadata} from "../src/ENSMetadata.sol";
-
-contract ENSVerifiedContract is ENSMetadata {
-    constructor() ENSMetadata() {}
-}
--- examples/SimpleMetadataContract.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
-
-import {ENSMetadata} from "../src/ENSMetadata.sol";
-
-contract SimpleMetadataContract is ENSMetadata {
-    constructor() ENSMetadata() {}
-}
