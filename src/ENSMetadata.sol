7855b44e1553ca254ef65d35776cf6fefd54ec7e
uzzol <uzzol.sse@gmail.com>
2024-11-13 20:31:15 +0600

    removed ownership from ensMetadata contract

--- src/ENSMetadata.sol
+++ src/ENSMetadata.sol
@@ -10,82 +10,51 @@ It uses the MetadataLib and ENSVerificationLib to handle the logic for managing 
 */
 interface IENSRegistry {
     function owner(bytes32 node) external view returns (address);
-
     function resolver(bytes32 node) external view returns (address);
 }
-// TODO: replace string with bytes32?
 
-/// @title ENS Metadata Contract
+/// @title ENS Metadata Manangement Contract
 /// @notice Manages metadata for a contract and verifies its associated ENS name.
-/// @dev Uses the ENSVerificationLib library to verify the ENS name.
+/// @dev Designed as a standard for associating ENS names with smart contract metadata.
+/// @author Bloclabs LLC
 contract ENSMetadata {
     using MetadataLib for MetadataLib.Metadata;
 
     MetadataLib.Metadata public metadata;
-    address public owner;
     address public ensRegistry;
 
-    event MetadataUpdated(
-        string title,
-        string description,
-        string ENS_name,
-        bool verification
-    );
+    event MetadataUpdated(string title, string description, string ENS_name, bool verification);
 
-    constructor(
-        string memory _title,
-        string memory _description,
-        string memory _ENS_name,
-        address _ensRegistry
-    ) {
+    constructor(string memory _title, string memory _description, string memory _ENS_name, address _ensRegistry) {
         // Update the metadata with the new values
         metadata.setMetadata(_title, _description, _ENS_name);
-        owner = msg.sender; // Set the contract deployer as the owner
         ensRegistry = _ensRegistry; // Set the ENS registry address based on the blockchain and environment
     }
 
-    modifier onlyOwner() {
-        // Ensure that the caller is the owner
-        require(msg.sender == owner, "Only the owner can modify the metadata");
-        _;
-    }
-
-    function setMetadata(
-        string memory _title,
-        string memory _description,
-        string memory _ENS_name
-    ) public onlyOwner {
-        // Update the metadata with the new values
-        // Automatically resets verification
-        metadata.setMetadata(_title, _description, _ENS_name);
-    }
-
+    /// @notice Verifies that the ENS name is owned by the caller and resolves to this contract.
     function verifyENS() external returns (bool) {
         // Call the verification function in the library
-        bool verification = ENSVerificationLib.verifyENS(
-            ensRegistry,
-            metadata.ENS_name,
-            address(this),
-            msg.sender
-        );
+        bool verification = ENSVerificationLib.verifyENS(ensRegistry, metadata.ENS_name, address(this), msg.sender);
 
         // If verification passes, set verification status to true
         metadata.verification = verification;
 
         return verification;
     }
+    
+    /// @notice Sets the metadata for the contract with a title, description, and ENS name.
+    /// @dev This function updates the metadata and emits an event.
+    /// It should only be called by the contract owner.
+    function setMetadata(string memory _title, string memory _description, string memory _ENS_name) public {
+        metadata.setMetadata(_title, _description, _ENS_name);
+    }
 
+    /// @notice Retrieves the current metadata.
     function getMetadata()
         public
         view
-        returns (
-            string memory title,
-            string memory description,
-            string memory ENS_name,
-            bool verification
-        )
+        returns (string memory title, string memory description, string memory ENS_name, bool verification)
     {
-        // Return the metadata
         return metadata.getMetadata();
     }
 }
